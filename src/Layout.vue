375d0d0af2759e8831c7c9d0ef71c4ea159379b1
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2022-02-12 12:20:43 +0300

    Update Layout.vue

--- src/Layout.vue
+++ src/Layout.vue
@@ -1,18 +1,16 @@
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
-	components: { Header, Footer }
-}
+	components: { Header, Footer },
+};
 </script>
 
 <template>
 	<Header />
 
-	<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center">
-		<router-view></router-view>
-	</div>
+	<router-view></router-view>
 
 	<Footer />
 </template>
