a6fdb5349c148c083268972530a1a8fd09224ee3
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2022-02-12 12:20:27 +0300

    about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,7 +2,56 @@
 </script>
 
 <template>
-	<section class="text-gray-600 body-font relative">
-		<div class="container px-5 py-12 mx-auto">about</div>
+	<section
+		class="
+			text-gray-600
+			body-font
+			relative
+			px-8
+			pt-8
+			pb-0
+			mx-auto
+			sm:max-w-l
+			md:max-w-full
+			lg:max-w-screen-lg
+			md:px-24
+			lg:px-8 lg:pt-10
+			text-justify
+		"
+	>
+		<p class="text-gray-700 md:text-lg mt-3">
+			Hi, i'm Berkin. I am a front-end developer located in Istanbul/Turkey. I'm a minimalist, open sourcerer, a vim
+			user. I use Arch <small class="opacity-40">(Linux)</small> by the way.
+		</p>
+
+		<h1 class="inline-block max-w-lg text-2xl font-extrabold tracking-tight text-black sm:text-4xl mt-6">Early Life</h1>
+
+		<p class="text-gray-700 md:text-lg mt-3">
+			My dream as a child was being a game developer. I was dreaming of building a browser-based MMO. I first used
+			Dreamweaver to build websites but then i quickly realized it's impossible to build a game with it and started
+			learning about web development from scratch. I followed lots of PSD to HTML tutorials and learnt about algorithms
+			with JavaScript.
+		</p>
+
+		<h1 class="inline-block max-w-lg text-2xl font-extrabold tracking-tight text-black sm:text-4xl mt-6">
+			High School
+		</h1>
+
+		<p class="text-gray-700 md:text-lg mt-3">
+			I left high school at 10th grade to have more time to build games and learn English. I got pretty good at English
+			and started using Unity to build games. I published over 10 games, 3 of which being in Google Play. I was still
+			learning web development in the meantime, svelte was my go-to framework and it was really easy to switch to Vue
+			after Vue 3 came out.
+		</p>
+
+		<h1 class="inline-block max-w-lg text-2xl font-extrabold tracking-tight text-black sm:text-4xl mt-6">University</h1>
+
+		<p class="text-gray-700 md:text-lg mt-3">
+			I studied at Information Security program of Istanbul Gedik University in 2019. Because of the pandemic, we
+			transitioned to distant learning after one semester i started my study. I completed my internship at Gedik Holding
+			in the first year of my study and i started working as a full-time Jr. Front-End Developer there in the second
+			year (April 2021). I graduated my associate degree and started studying Management Information Systems in Anadolu
+			University in .
+		</p>
 	</section>
 </template>
