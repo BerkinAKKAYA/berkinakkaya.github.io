c701726513ea418c1f7f7ecec679f63f900fe179
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2022-02-13 10:11:51 +0300

    contact page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,5 +1,32 @@
 <template>
-	<section class="text-gray-600 body-font relative">
-		<div class="container px-5 py-12 mx-auto">contact</div>
-	</section>
+	<div class="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-sm md:px-24 lg:px-8 lg:pt-20">
+		<div class="grid row-gap-8 sm:row-gap-0 md:grid-cols-2">
+			<div class="p-8">
+				<div class="max-w-md text-center">
+					<div
+						class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-16 sm:h-16"
+					>
+						<i class="fas fa-phone"></i>
+					</div>
+					<h6 class="mb-2 font-semibold leading-5">Phone</h6>
+					<p class="mb-3 text-sm text-gray-900">
+						<a href="tel:+90 538 284 8787">+90 538 284 8787</a>
+					</p>
+				</div>
+			</div>
+			<div class="p-8">
+				<div class="max-w-md text-center">
+					<div
+						class="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-16 sm:h-16"
+					>
+						<i class="fas fa-envelope"></i>
+					</div>
+					<h6 class="mb-2 font-semibold leading-5">Mail</h6>
+					<p class="mb-3 text-sm text-gray-900">
+						<a href="mailto:berkin_akkaya@hotmail.com">berkin_akkaya@hotmail.com</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
